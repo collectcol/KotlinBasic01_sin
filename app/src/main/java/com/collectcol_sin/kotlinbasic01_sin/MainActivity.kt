0e3f64182d7ecda328d25084ba47ae2de78e9bbb
collectcol <sinwookyoung15@gmail.com>
2023-01-29 17:13:29 +0900

    버튼 두개 추가

--- app/src/main/java/com/collectcol_sin/kotlinbasic01_sin/MainActivity.kt
+++ app/src/main/java/com/collectcol_sin/kotlinbasic01_sin/MainActivity.kt
@@ -2,6 +2,7 @@ package com.collectcol_sin.kotlinbasic01_sin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity()
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickButton
+        smallButton
+
     }
 }
